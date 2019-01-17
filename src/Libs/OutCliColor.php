4a16432f53851ff44f868d052f711bd0bf906e5f
xingbo.li <1711293058@qq.com>
2019-01-17 14:54:48 +0800

    outlicolor

--- src/Libs/OutCliColor.php
+++ src/Libs/OutCliColor.php
@@ -11,13 +11,34 @@ namespace Ddup\Part\Libs;
 
 class OutCliColor
 {
+    public static function red()
+    {
+        return '1;31';
+    }
+
     public static function green()
     {
         return '1;32';
     }
 
-    public static function red()
+    public static function orange()
     {
-        return '1;31';
+        return '1;33';
     }
+
+    public static function blue()
+    {
+        return '1;34';
+    }
+
+    public static function purple()
+    {
+        return '1;35';
+    }
+
+    public static function white()
+    {
+        return '1;37';
+    }
+
 }
